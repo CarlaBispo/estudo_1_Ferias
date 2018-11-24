2f13ada2c26553b4b8e0af4db5f32809bef5956d
Carla Bispo <carlabispo@gmail.com>
2018-11-24 18:19:48 -0200

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float move = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float h = Input.GetAxis("Horizontal");
+        float xPos = h * move;
+        transform.position = new Vector3(xPos, .5f, 0);
+
+    }
+}
---
+++ prefabScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefabScript : MonoBehaviour {
+
+    public Transform cubeEnd;
+    public Rigidbody cubePrefab;
+    
+
+	// Use this for initialization
+	void Start () {
+
+       
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody cubeInstance;
+            cubeInstance = Instantiate
+                (cubePrefab, cubeEnd.position, cubeEnd.rotation) 
+                as Rigidbody;
+
+            cubeInstance.AddForce(cubeEnd.forward * 2000);
+
+           
+        }
+
+        
+		
+	}
+}
